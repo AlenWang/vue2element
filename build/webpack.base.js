f3ec37fda02059018508e631a5c2354578f8bf0e
刘星星 <liuxingxing@liuxingxingdeMacBook-Pro.local>
2016-11-07 19:02:33 +0800

    修改打包后的路径问题

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../dist/assets'),
     filename: '[name].js',
-    publicPath: '/assets/'
+    publicPath: 'assets/'
   },
   resolve: {
     extensions: ['', '.js', '.vue', '.css', '.json'],
